5745cfdb5b9ba3514dfd08acfb7c1bac2c953515
System Administrator <root@INBGML0168.local>
2024-02-19 14:49:04 +0530

    Ordeer

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ A = input("are u hungry?")
 if A == 'Yes':
     print("Go Eat pizza")
     print("And yeah also eat burger dear")
+    print("Or Order something u needed!")
 else:
     print("Then shut and go Study")
