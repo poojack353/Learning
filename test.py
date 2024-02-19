0890c777593b450c9ea2546083034bd809ce7761
System Administrator <root@INBGML0168.local>
2024-02-19 15:32:26 +0530

    Music vs Netflix

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-A = input("are u hungry?")
-if A == 'Yes':
-    print("Go Eat pizza")
-    print("And yeah also eat burger dear")
-    print("Or Order something u needed!")
-else:
-    B = input("Are you thirsty?")
-    if B == "Yes":
-        print("Drink Coke")
-    else:
-        print("Shut and read then")
+A = input("Oye do u like music or Netflix to spend ur free time?")
+if A == "Music":
+    print("Wow good even im music lover")
+elif A == "Netflix":
+    print("Oh Nice. Did u watched FRIENDS show?")
+    print("Yes i did")
+
